62f52104e721b17b9e18e556cbb818076808ca61
ivan-pl <pleskaiv@gmail.com>
2023-01-12 10:11:02 +0300

    build: fix linters and run them

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
+    "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "jest"],
   rules: {},
 };
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # js-pro-hw-1
+
 maxItemAssociation
